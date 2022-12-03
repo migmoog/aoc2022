0b3f328153123ae2353932d33e3ae745ab813e8c
migmoog <57271984+migmoog@users.noreply.github.com>
2022-12-02 21:58:00 -0500

    Initial

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod day2;
+
+use std::env::consts::OS;
+use day2::day_2;
+
+fn main() {
+    day_1(include_str!("day1.txt"));
+    day_2(include_str!("day2.txt"));
+}
+
+fn day_1(input: &str) {
+    // part 1
+    let group_strs = input.split(
+        if OS == "windows" {
+            // windows whyyyyy
+            "\r\n\r\n"
+        } else { "\n\n" });
+
+    let mut groups = Vec::new();
+    for g in group_strs {
+        let total_elf_cals: i32 = g.lines().map(
+            |s| s.parse::<i32>().unwrap()
+        ).sum();
+        groups.push(total_elf_cals);
+    }
+
+    groups.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    println!("Day 1 (part 1): {}", groups[0]);
+    println!("Day 1 (part 2): {}", groups[0..3].iter().sum::<i32>());
+}
